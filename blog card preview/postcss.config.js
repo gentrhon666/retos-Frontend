e201efe9fb188592437281ffcca5ad9216a2209e
gentrhon666 <65492055+gentrhon666@users.noreply.github.com>
2024-06-18 00:07:57 +0200

    Delete blog card preview directory

--- blog card preview/postcss.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")],
-  cssnano: { preset: "default" },
-  ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
-};
